109ae643d546c858480fcbfebc36d615053aabcb
Angel <annoch22@gmail.com>
2023-09-05 21:01:31 +0200

    Typescript cd

--- src/App.tsx
+++ src/App.tsx
@@ -1,95 +1,21 @@
-import { useState } from "react";
 import "./App.css";
+import Exercise1 from "./Exercise/Exercise-1";
+import Exercise2 from "./Exercise/Exercise-2";
+import Exercise3 from "./Exercise/Exercise-3";
 
-import { processString } from "./Exercise/Exercise-1";
-
-// Implicit typing
-const x = 1;
-
-// Explicit typing
-const y: number = 2;
-
-// Incorrect JS
-// let x = 1;
-// x = 'abc';
-// x = new Date();
-
-// Type number
-const numberType: number = 2000;
-// Type string
-const stringType: string = `I'm a string`;
-// Type boolean
-const booleanType: boolean = true;
-// Type array | Array<number> === number[]
-const arrayType: Array<number | string> = [numberType, 1, 121, stringType];
-// Type tuple
-const tupleType: [number, string] = [1, "string"];
-// Type enum
-enum Suit {
-  Spades = "Spades",
-  Hearts = "Hearts",
-  Diamonds = "Diamonds",
-  Clubs = "Clubs",
-}
-const cardsSpades: Suit = Suit.Spades;
-// Type any
-// let x: any = 4;
-// x = 'a';
-// x = new Date();
-// Type void
-const consoleLogger = (message: string): void => console.log(message);
+const person = {
+  name: "Antoni",
+  age: 90,
+  isAdmin: false,
+};
 
 function App() {
-  const [count, setCount] = useState(121);
-
-  function roundNumber(num: number): number {
-    return Number(num.toFixed(2));
-  }
-
-  function sumTwoNumbers(a: number, b: number): number {
-    const result = a + b;
-    const roundedResult = roundNumber(result);
-
-    return roundedResult;
-  }
-
-  consoleLogger("Show me in console");
-
-  console.log(processString("abc"));
-
   return (
     <>
-      <h1>Typescript exercises</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-
-        <p>
-          {x} | {y}
-        </p>
-        <p>{sumTwoNumbers(2, 4)}</p>
-        <p>
-          {numberType} - {typeof numberType}
-        </p>
-        <p>
-          {stringType} - {typeof stringType}
-        </p>
-        <p>
-          {booleanType} - {typeof booleanType}
-        </p>
-        <p>
-          {arrayType} - {typeof arrayType}
-        </p>
-        <p>
-          {tupleType} - {typeof tupleType}
-        </p>
-        <p>
-          {cardsSpades} - {typeof cardsSpades}
-        </p>
-        <p>{Suit.Hearts}</p>
-        <p>{roundNumber(9999)}</p>
-      </div>
+      <h1>Typescript training</h1>
+      <Exercise1 />
+      <Exercise2 />
+      <Exercise3 name={person.name} />
     </>
   );
 }
--- src/Exercise/Exercise-1.tsx
+++ src/Exercise/Exercise-1.tsx
@@ -1,26 +1,58 @@
+const INPUT_STRING = "Hello world!!!!!";
+
+function roundNumber(num: number): number {
+  return Number(num.toFixed(2));
+}
+
+function sumTwoNumbers(a: number, b: number): number {
+  const result = a + b;
+  const roundedResult = roundNumber(result);
+
+  return roundedResult;
+}
+
 // 1. funkcję countCharacters liczącą ilość znaków w stringu
-export function countCharacters(str: string): number {
-  return str.length;
+function countCharacters(string: string): number {
+  return string.length;
 }
 
 // 2. funkcję isEven mówiącą czy liczba jest parzysta czy nie (zwracająca boolean)
-
-export function isEven(num: number): boolean {
-  return num % 2 === 0;
+function isEven(number: number): boolean {
+  return number % 2 === 0;
 }
 
 // 3. funkcję getParityMessage która na podstawie boolean zwraca komunikat ta liczba jest parzysta/ta liczba nie jest parzysta
+function getParityMessage(isEven: boolean): string {
+  //   if (isEven) {
+  //     return 'This number is even';
+  //   }
 
-export function getParityMessage(isEven: boolean): string {
-  return isEven ? "Ta liczba jest parzysta" : "Ta liczba nie jest parzysta";
+  //   return 'This number is odd';
+
+  return isEven ? "This number is even" : "This number is odd";
 }
 
 // 4. funkcję processString która na podstawie stringa, wypisuje go, wypisuje liczbę jego znaków oraz informację czy liczba znaków jest/nie jest parzysta. Funkcja ta powinna wywoływać 3 powyższe funkcje (zwraca string)
+function processString(str: string): string {
+  const stringLength = countCharacters(str);
+  const isEvenLength = isEven(stringLength);
+  const isEvenMessage = getParityMessage(isEvenLength);
 
-export function processString(str: string): string {
-  const charactersCount = countCharacters(str);
-  const isEvenCharactersCount = isEven(charactersCount);
-  const parityMessage = getParityMessage(isEvenCharactersCount);
-
-  return `${str} | ${charactersCount} | ${parityMessage}`;
+  return `${str} - characters: ${stringLength} - ${isEvenMessage}`;
 }
+
+function Exercise1() {
+  return (
+    <div className="card">
+      <h2>Exercise 1</h2>
+      <p>Sum two numbers - {sumTwoNumbers(2.9999, 4.2)}</p>
+      <p>Round number - {roundNumber(9999.8877)}</p>
+      <p>Count characters - {countCharacters("String to be counted")}</p>
+      <p>Number is even - {isEven(2).toString()}</p>
+      <p>Is 3 even number - {getParityMessage(isEven(3))}</p>
+      <p>{processString(INPUT_STRING)}</p>
+    </div>
+  );
+}
+
+export default Exercise1;
